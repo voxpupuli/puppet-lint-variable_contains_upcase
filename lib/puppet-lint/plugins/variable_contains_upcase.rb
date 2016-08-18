07f06a63197e6fb7cbd8128e6685673f74fa7e50
Mark McKinstry <mmckinst@nexcess.net>
2016-08-18 14:44:23 -0400

    get rid of VARIABLE_TYPES
    it was removed/renamed in
    https://github.com/rodjek/puppet-lint/pull/483/ which is now in
    puppet-lint 2.0.1

--- lib/puppet-lint/plugins/variable_contains_upcase.rb
+++ lib/puppet-lint/plugins/variable_contains_upcase.rb
@@ -1,7 +1,7 @@
 PuppetLint.new_check(:variable_contains_upcase) do
   def check
     tokens.select { |r|
-      VARIABLE_TYPES.include? r.type
+      Set[:VARIABLE, :UNENC_VARIABLE].include? r.type
     }.each do |token|
       if token.value.match(/[A-Z]/)
         notify :warning, {
