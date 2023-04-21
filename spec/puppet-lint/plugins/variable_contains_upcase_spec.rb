9c6df48eeb092c0bc2d7c12a29a61f48f6d737d9
Tim Meusel <tim@bastelfreak.de>
2023-04-21 17:31:22 +0200

    rubocop: autofix

--- spec/puppet-lint/plugins/variable_contains_upcase_spec.rb
+++ spec/puppet-lint/plugins/variable_contains_upcase_spec.rb
@@ -6,11 +6,11 @@ describe 'variable_contains_upcase' do
   context 'a variable containing a capital' do
     let(:code) { '$fOobar' }
 
-    it 'should only detect a single problem' do
+    it 'onlies detect a single problem' do
       expect(problems).to have(1).problem
     end
 
-    it 'should create a warning' do
+    it 'creates a warning' do
       expect(problems).to contain_warning(msg).on_line(1).in_column(1)
     end
   end
@@ -18,11 +18,11 @@ describe 'variable_contains_upcase' do
   context 'variable containing a capital' do
     let(:code) { '" $fOobar"' }
 
-    it 'should only detect a single problem' do
+    it 'onlies detect a single problem' do
       expect(problems).to have(1).problem
     end
 
-    it 'should create a warning' do
+    it 'creates a warning' do
       expect(problems).to contain_warning(msg).on_line(1).in_column(3)
     end
   end
@@ -39,15 +39,15 @@ describe 'variable_contains_upcase' do
     context 'fix variable containing a capital' do
       let(:code) { '" $fOobar"' }
 
-      it 'should only detect a single problem' do
+      it 'onlies detect a single problem' do
         expect(problems).to have(1).problem
       end
 
-      it 'should fix the manifest' do
+      it 'fixes the manifest' do
         expect(problems).to contain_fixed(msg).on_line(1).in_column(3)
       end
 
-      it 'should downcase the variable name' do
+      it 'downcases the variable name' do
         expect(manifest).to eq('" $foobar"')
       end
     end
@@ -55,21 +55,23 @@ describe 'variable_contains_upcase' do
     context 'a hash with an upcase key should not fail' do
       let(:code) { ' $myhash["Foo"]' }
 
-      it 'should not detect a single problem' do
+      it 'does not detect a single problem' do
         expect(problems).to have(0).problem
       end
     end
+
     context 'a hash with an upcase key should not fail' do
       let(:code) { ' $myhash["Foo"]["bAr"]' }
 
-      it 'should not detect a single problem' do
+      it 'does not detect a single problem' do
         expect(problems).to have(0).problem
       end
     end
+
     context 'a hash variable with an upcase key should fail' do
       let(:code) { ' $myHash["foo"]["bar"]' }
 
-      it 'should detect a single problem' do
+      it 'detects a single problem' do
         expect(problems).to have(1).problem
       end
     end
